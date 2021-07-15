7114b1ef8253b772ce11a39aa36e576284c3fd54
Susumu Hayami <skyinthehand17@gmail.com>
2021-07-15 16:47:15 +0900

    (#1) expressから表示を返す

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,10 @@
 import * as functions from "firebase-functions";
+import * as express from "express";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+const app = express();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
+exports.app = functions.https.onRequest(app);
