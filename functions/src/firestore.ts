0c88a7c06380055136b88f55f8b48d93079fba5f
Susumu Hayami <skyinthehand17@gmail.com>
2021-07-18 01:56:20 +0900

    (#14) マッチングデータの作成

--- functions/src/firestore.ts
+++ functions/src/firestore.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import { v4 as uuid } from "uuid";
+import * as moment from "moment-timezone";
 
 export interface IUserData {
   createdDate?: Date;
@@ -17,6 +18,35 @@ export interface IUserSeasonData {
   userId: string;
   seasonId: string;
   rate: number;
+}
+
+const EMatchResult = {
+  Win: 1,
+  Lose: 0,
+  Cancel: -1,
+};
+export type EMatchResult = typeof EMatchResult[keyof typeof EMatchResult];
+
+export interface IUserMatchData {
+  userId: string;
+  reportedResult?: EMatchResult;
+  diffRate: number;
+}
+
+const EMatchStatus = {
+  Completion: 1,
+  Progress: 0,
+  Cancel: -1,
+};
+export type EMatchStatus = typeof EMatchStatus[keyof typeof EMatchStatus];
+
+export interface IMatchData {
+  seasonId: string;
+  user0MatchData: IUserMatchData;
+  user1MatchData?: IUserMatchData;
+  createdDate: Date;
+  updatedDate: Date;
+  status: EMatchStatus;
 }
 
 export const INITIAL_RATE = 1500;
@@ -158,4 +188,65 @@ export async function createUserSeason(
   };
   await userSeasonsRef.doc(userSeasonId).set(userSeasonData);
   return userSeasonId;
+}
+
+/**
+ * Get match data
+ * @param {string} userId
+ * @param {string} seasonId
+ * @return {Promise<IMatchData | null>}
+ */
+export async function getMatchByUserIdAndSeasonId(
+  userId: string,
+  seasonId: string
+): Promise<IMatchData | null> {
+  const db = admin.firestore();
+  const matchsRef = db.collection("matchs");
+  const query1Snapshot = await matchsRef
+    .where("userId0.userId", "==", userId)
+    .where("seasonId", "==", seasonId)
+    .get();
+  if (query1Snapshot.docs.length > 0) {
+    const doc = query1Snapshot.docs[0];
+    return doc.data() as IMatchData;
+  }
+  const query2Snapshot = await matchsRef
+    .where("userId1.userId", "==", userId)
+    .where("seasonId", "==", seasonId)
+    .get();
+  if (query2Snapshot.docs.length > 0) {
+    const doc = query2Snapshot.docs[0];
+    return doc.data() as IMatchData;
+  }
+  return null;
+}
+
+/**
+ * Create match data
+ * @param {string} userId
+ * @param {string} seasonId
+ * @param {Date} date
+ * @return {Promise<string | null>}
+ */
+export async function createMatch(
+  userId: string,
+  seasonId: string
+): Promise<IMatchData> {
+  const db = admin.firestore();
+  const matchsRef = db.collection("matchs");
+  const userMatchData: IUserMatchData = {
+    userId,
+    diffRate: 0,
+  };
+  const currentTime = moment.tz("Asia/Tokyo").toDate();
+  const matchData: IMatchData = {
+    seasonId,
+    user0MatchData: userMatchData,
+    createdDate: currentTime,
+    updatedDate: currentTime,
+    status: EMatchStatus.Progress,
+  };
+  const matchId = uuid();
+  await matchsRef.doc(matchId).set(matchData);
+  return matchData;
 }
--- functions/src/match.ts
+++ functions/src/match.ts
@@ -1,15 +1,36 @@
 import * as express from "express";
 
+import * as firestore from "./firestore";
+
 // eslint-disable-next-line new-cap
 export const router = express.Router();
 
 router.get("/", (req, res) => {
-  if (!req.session || !req.session.passport || !req.session.userId) {
+  if (
+    !req.session ||
+    !req.session.passport ||
+    !req.session.userId ||
+    !req.session.seasonId
+  ) {
     res.redirect("/");
     return;
   }
 
-  res.render("match", {
-    user: req.session.passport.user,
+  const userId = req.session.userId;
+  const seasonId = req.session.seasonId;
+  firestore.getMatchByUserIdAndSeasonId(userId, seasonId).then((matchData) => {
+    if (matchData !== null) {
+      res.render("match", {
+        matchData: matchData,
+      });
+      return;
+    }
+
+    firestore.createMatch(userId, seasonId).then((matchData) => {
+      res.render("match", {
+        matchData: matchData,
+      });
+      return;
+    });
   });
 });
